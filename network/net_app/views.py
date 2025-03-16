9a7218eee7891216f39a1c72743b354cb14a68ce
phbruemmer <phbruemmer@gmail.com>
2025-03-16 22:15:49 +0100

    - User inputs can now be processed in the ipv4_test_view

--- network/net_app/views.py
+++ network/net_app/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 
 
 def ipv4_test_view(request):
+    input_fields = {
+        "network_address": None,
+        "host_portions": None,
+        "broadcast_address": None,
+        "max_hosts": None,
+        "address_range_from": None,
+        "address_range_to": None,
+    }
+
+    if request.method == 'POST':
+        for field in input_fields:
+            input_fields[field] = request.POST.get(field)
+        print(input_fields)
     return render(request, "ipv4.html")
 
