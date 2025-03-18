0f5b2ff02413ea946956fa2d6fd07e6fee10a38b
phbruemmer <phbruemmer@gmail.com>
2025-03-18 20:52:11 +0100

    - minor changes in the ipv4 test view due to changes in the prvt module scripts

--- network/net_app/urls.py
+++ network/net_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ipv4_test_view
 
 urlpatterns = [
-    path('ipv4/', ipv4_test_view)
+    path('ipv4/', ipv4_test_view),
 ]
--- network/net_app/views.py
+++ network/net_app/views.py
@@ -1,10 +1,14 @@
+from Scripts.bottle import redirect
 from django.shortcuts import render
 
 from .prvt_modules.custom import Network
+from .prvt_modules.network_checks import check_data
 
 
 def ipv4_test_view(request):
     input_fields = {
+        "ip": None,
+        "subnet_mask": None,
         "network_address": None,
         "host_portions": None,
         "broadcast_address": None,
@@ -13,16 +17,23 @@ def ipv4_test_view(request):
         "address_range_to": None,
     }
     network = Network()
-    network.create_random_ip_from_subnet()
-
-    args = {
-        'IP': network.ip,
-        'SUBNETMASK': network.subnet_mask,
-    }
 
     if request.method == 'POST':
         for field in input_fields:
             input_fields[field] = request.POST.get(field)
-        print(input_fields)
-    return render(request, "ipv4.html", args)
 
+        network.ip = input_fields["ip"]
+        network.subnet_mask = input_fields["subnet_mask"]
+        network.check_values()
+        network.create_dict()
+
+        correct_data = check_data(input_fields, network.dict)
+    else:
+        network.create_random_ip_from_subnet()
+
+    args = {
+        'IP': network.ip,
+        'SUBNET_MASK': network.subnet_mask,
+    }
+
+    return render(request, "ipv4.html", args)
