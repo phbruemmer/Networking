2f5293b10d54c9b59563470dc407393747e4f38e
phbruemmer <phbruemmer@gmail.com>
2025-03-18 23:00:23 +0100

    - reworked the ipv4_test_view -view. - We can now properly receive input data from the user, process it and return the validation of the input

--- network/net_app/views.py
+++ network/net_app/views.py
@@ -1,39 +1,58 @@
-from Scripts.bottle import redirect
 from django.shortcuts import render
-
 from .prvt_modules.custom import Network
 from .prvt_modules.network_checks import check_data
 
 
 def ipv4_test_view(request):
-    input_fields = {
-        "ip": None,
-        "subnet_mask": None,
-        "network_address": None,
-        "host_portions": None,
-        "broadcast_address": None,
-        "max_hosts": None,
-        "address_range_from": None,
-        "address_range_to": None,
-    }
-    network = Network()
+    def process_inputs():
+        input_fields = {
+            "ip": None,
+            "subnet_mask": None,
+            "network_address": None,
+            "host_portions": None,
+            "broadcast_address": None,
+            "max_hosts": None,
+            "address_range_from": None,
+            "address_range_to": None,
+        }
 
-    if request.method == 'POST':
         for field in input_fields:
+            print(field)
+            if field is None:
+                return
             input_fields[field] = request.POST.get(field)
-
         network.ip = input_fields["ip"]
         network.subnet_mask = input_fields["subnet_mask"]
+
         network.check_values()
         network.create_dict()
 
         correct_data = check_data(input_fields, network.dict)
+        correct_data.pop(0)
+        correct_data.pop(0)
+        args['correct_data'] = correct_data
+
+    args = {}
+
+    network = Network()
+
+    if request.method == 'POST':
+        btn_action = request.POST.get('action')
+        if btn_action == 'check':
+            process_inputs()
+        elif btn_action == 'next':
+            network.create_subnet_mask()
+            network.create_random_ip_from_subnet()
+        else:
+            args.update({
+                'IP': "Invalid Input - Try again!"
+            })
     else:
         network.create_random_ip_from_subnet()
 
-    args = {
+    args.update({
         'IP': network.ip,
         'SUBNET_MASK': network.subnet_mask,
-    }
+    })
 
     return render(request, "ipv4.html", args)
