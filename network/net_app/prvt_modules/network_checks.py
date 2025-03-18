1a583f52bf40714256b630933676cbeeda11edcd
phbruemmer <phbruemmer@gmail.com>
2025-03-18 20:51:27 +0100

    - Added network_checks.py to check which elements of a hashmap are identical

---
+++ network/net_app/prvt_modules/network_checks.py
@@ -0,0 +1,10 @@
+def check_data(input_dict, output_dict):
+    correct_data = []
+    for key in input_dict:
+        print(f"{key} : {input_dict[key]} : {output_dict[key]}")
+        if input_dict[key] == output_dict[key]:
+            correct_data.append(True)
+        else:
+            correct_data.append(False)
+    return correct_data
+
