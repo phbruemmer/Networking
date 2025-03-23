dc761ac2c57e1a2487939d412b7358a376970441
phbruemmer <phbruemmer@gmail.com>
2025-03-23 21:14:12 +0100

    - Created ipv6 class to create random mac addr., random ipv6 addr., private ipv6 addr., random link local addr, link local addr. based up on the mac addr. and acheck_ipv6 method to check whether an ipv6 address is in the correct format

--- network/net_app/prvt_modules/custom.py
+++ network/net_app/prvt_modules/custom.py
@@ -124,7 +124,122 @@ class Network:
         return self.dict
 
 
+class Ipv6:
+    def __init__(self):
+        self.ipv6 = None
+        self.MAC = None
+
+    def create_MAC(self):
+        self.MAC = ":".join(f"{random.randint(0, 255):02x}" for _ in range(6))
+        return self.MAC
+
+    def create_random(self):
+        self.ipv6 = str(ipaddress.IPv6Address(random.getrandbits(128)))
+        return self.ipv6
+
+    def create_private(self):
+        prefix = "fd"
+        rest = ":".join(f"{random.randint(0, 65535):x}" for _ in range(7))
+        self.ipv6 = f"{prefix}00::{rest}"
+        return self.ipv6
+
+    def create_link_local(self):
+        prefix = "FE80:" + ":".join(["0" * 4] * 3)
+        suffix = ":".join(f"{random.randint(0, 65535):04x}" for _ in range(4))
+        self.ipv6 = f"{prefix}:{suffix}"
+        return self.ipv6
+
+    def create_link_local_based_on_mac(self):
+        if self.MAC is None:
+            self.create_MAC()
+        split_mac = self.MAC.split(':')
+        prefix = "fe80:" + ":".join(["0" * 4] * 3)
+        ### merge two byte segments and add ff:fe into the center ###
+        split_mac[2] += "ff"
+        split_mac[3] = "fe" + split_mac[3]
+        split_mac[4] += split_mac[5]
+        split_mac.pop(5)
+        ### calculate full suffix ###
+        suffix = f"{(int(split_mac[0], 16) ^ 0x02):02x}" + ":".join(split_mac[1:])
+
+        self.ipv6 = f"{prefix}:{suffix}"
+        return self.ipv6
+
+    def check_ipv6(self):
+        if self.ipv6 is None:
+            raise ValueError("No ipv6 address given!")
+
+        split_ip = self.ipv6.split(':')
+        ip_size = len(split_ip)
+        double_colons = self.ipv6.count('::')
+
+        if ip_size == 1 or double_colons > 1:
+            return False
+
+        prev_char = ""
+        consecutive_colons = 0
+
+        for char in self.ipv6:
+            if prev_char == ":" and char == ":":
+                consecutive_colons += 1
+                if consecutive_colons > 1:
+                    return False
+            prev_char = char
+
+        if double_colons == 0 and not ip_size == 8:
+            return False
+        elif double_colons == 1 and (not ip_size <= 8 or not ip_size > 1):
+            return False
+
+        for segment in split_ip:
+            if segment == "":
+                continue
+            try:
+                if not (0 <= int(segment, 16) <= 65535):
+                    return False
+            except ValueError:
+                return False
+        return True
+
+
 if __name__ == '__main__':
-    network = Network()
+    """network = Network()
     network.subnet_mask = "255.255.192.0"
-    network.calc_max_number_of_hosts()
+    network.calc_max_number_of_hosts()"""
+
+    valid_ipv6_addresses = [
+        "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+        "2001:db8:85a3::8a2e:370:7334",
+        "::1",
+        "fe80::0202:b3ff:fe1e:8329",
+        "2001:db8::ff00:42:8329",
+        "2001:0db8:0000:0000:0000:0000:0000:0001",
+        "2001:db8:abcd:ef01:2345:6789:abcd:ef01",
+        "::abcd:1234:5678:9abc",
+        "2001:db8:0:0:0:0:0:1",
+        "::1234:5678:9abc:def0"
+    ]
+
+    invalid_ipv6_addresses = [
+        "2001:db8:85a3::8a2e:370:7334::",
+        "::12345::5678",
+        "gggg:gggg:gggg:gggg:gggg:gggg:gggg:gggg",
+        "1234:5678:abcd:efgh:ijkl:mnop:qrst:uvwx",
+        "::z1f4:5678:9abc:def0",
+        "2001:db8:85a3:0000:0000:8a2e:0370:73344",
+        "2001:db8:85a3::8a2e::370:7334",
+        "::192.168.1.1",
+        "::2001:db8:85a3:0000:0000:8a2e:0370",
+        "::ffff:192.168.300.1""300"
+    ]
+
+    ipv6 = Ipv6()
+
+    for ip in valid_ipv6_addresses:
+        ipv6.ipv6 = ip
+        print(ipv6.check_ipv6())
+
+    for ip in invalid_ipv6_addresses:
+        ipv6.ipv6 = ip
+        print(ipv6.check_ipv6())
+
